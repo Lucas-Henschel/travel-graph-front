b663291b92282279cfdc8c3e4d23b38e9155dd30
LucasHenschel <lucas101henschel@gmail.com>
2026-06-09 22:23:52 -0300

    feat: register ToastService and add global Toast component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 import Aura from "@primeuix/themes/aura";
 
 import App from "./App.vue";
@@ -18,6 +19,7 @@ app.use(PrimeVue, {
     preset: Aura,
   },
 });
+app.use(ToastService);
 
 useAuthStore(pinia).hydrateSession();
 
